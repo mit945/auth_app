3864d7528e827851dbe037811d84bafed339cbd9
mit <mit945.mc@gmail.com>
2020-07-02 15:25:22 -0700

    update session store to choose production or localhost

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,11 @@
+if Rails.env == "production"
+
 Rails.application.config.session_store :cookie_store, key: "_auth_app", domain: "react-rails.heroku.com"
+
+else
+
+Rails.application.config.session_store :cookie_store, key: "_auth_app"
+
+end
+
+

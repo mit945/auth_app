cd614ed0695a8b0961f9d72bbb0357eab8cacafb
mit <mit945.mc@gmail.com>
2020-07-03 15:50:16 -0700

    fixing typo

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,10 +1,11 @@
 class RegistrationsController < ApplicationController
     def create 
       user = User.create!(
-            email: params['user]['email']
+            email: params['user']['email'],
             password: params['user']['password'],
             password_confirmation: params['user']['password_confirmation']
         ) 
+    
 
       if user 
         session[:user_id] = user.id
